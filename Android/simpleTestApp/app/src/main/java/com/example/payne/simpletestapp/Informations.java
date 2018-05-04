4d25ca1255da28e6e1ebac60e4ff3283aff84fd4
payne911 <payne911@users.noreply.github.com>
2018-05-04 13:47:13 -0400

    Merged epicness
    Accidently had to revert. Enjoy!

---
+++ Android/simpleTestApp/app/src/main/java/com/example/payne/simpletestapp/Informations.java
@@ -0,0 +1,13 @@
+package com.example.payne.simpletestapp;
+
+/**
+ * Informations des alertes à afficher
+ *
+ * Created by Utilisateur on 2018-05-03.
+ */
+
+public class Informations {
+
+    //TODO: Définir les différentes catégories
+
+}
---
+++ Android/simpleTestApp/app/src/main/java/com/example/payne/simpletestapp/MainActivity.java
@@ -0,0 +1,195 @@
+package com.example.payne.simpletestapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.util.BoundingBox;
+
+
+public class MainActivity extends AppCompatActivity implements MapEventsReceiver {
+
+    public static final double[] MONTREAL_COORD = {45.5161, -73.6568};
+
+    MapView map = null;
+    public MapDisplay myMap = null;
+    public MapEventsOverlay mapEventsOverlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //handle permissions first, before map is created. not depicted here
+
+/* REMOVED BECAUSE BUGGY AND USELESS! "Configuration" is for resuming saved preferences (or something like that...)
+
+        //load/initialize the osmdroid configuration, this can be done
+
+        final Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile
+        //servers will get you banned based on this string
+*/
+
+        //inflate and create the map
+        setContentView(R.layout.activity_main);
+
+        //creating the Toolbar?
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        map = findViewById(R.id.map);
+        myMap = new MapDisplay(map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        // set zoom control and multi-touch gesture
+        map.setBuiltInZoomControls(false);
+        map.setMultiTouchControls(true);
+
+        // default initial value
+        IMapController mapController = map.getController();
+        mapController.setZoom(12);
+        GeoPoint startPoint = new GeoPoint(MONTREAL_COORD[0], MONTREAL_COORD[1]);
+        mapController.setCenter(startPoint);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        /* TODO : Add "SEARCH", "FILTERS", and "HISTORIQUE"
+        https://developer.android.com/training/appbar/action-views */
+
+
+        mapEventsOverlay = new MapEventsOverlay(this, this);
+        map.getOverlays().add(0, mapEventsOverlay);
+    }
+
+    @Override
+    public boolean singleTapConfirmedHelper(GeoPoint p) {
+        Toast.makeText(this, "tapped :)", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint p) {
+        myMap.addPin(p);
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        switch (id) {
+            case (R.id.action_settings):
+                Toast.makeText(this, "Who fuckings cares...", Toast.LENGTH_SHORT).show();
+                /* TODO: Try a Standard Request here
+                https://hackqc.herokuapp.com/api/greeting?name=bob&annee=345&lastname=lolippop
+                https://developer.android.com/training/volley/request
+                https://stackoverflow.com/questions/3027066/how-to-send-a-json-object-over-request-with-android?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+                 */
+                break;
+
+            case (R.id.posBtn):
+                BoundingBox current = map.getBoundingBox();
+
+                double top = current.getLatNorth();
+                double bottom = current.getLatSouth();
+                double east = current.getLonEast();
+                double west = current.getLonWest();
+
+                String descr = "north : " + top + " \n" +
+                        "south : " + bottom + " \n" +
+                        "east :" + east + " \n" +
+                        "west : " + west;
+
+                Toast.makeText(this, descr, Toast.LENGTH_SHORT).show();
+                break;
+
+            case (R.id.centerBtn):
+                IGeoPoint center = map.getMapCenter();
+                Toast.makeText(this,
+                        "Center\n Lat : " + center.getLatitude() +
+                                "\nLong : " + center.getLongitude(),
+                        Toast.LENGTH_SHORT).show();
+
+            case (R.id.highlight):
+                myMap.highlightCurrent(findViewById(android.R.id.content));
+                break;
+
+            case (R.id.removeAll):
+                myMap.removeAll(findViewById(android.R.id.content), mapEventsOverlay);
+                break;
+
+            case (R.id.addPin):
+                myMap.addPin(myMap.getCenter());
+                break;
+
+            case (R.id.circleBtn):
+                myMap.drawCircleAtCenter(1000, 5);
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+}
