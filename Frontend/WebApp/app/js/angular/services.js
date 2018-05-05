e5596b0989e2f8039c0f6fc17fb66cae153393ae
CP Lepage <lepage.charlesphilippe@gmail.com>
2018-05-05 11:18:54 -0400

    frontend

--- Frontend/WebApp/app/js/angular/services.js
+++ Frontend/WebApp/app/js/angular/services.js
@@ -2,6 +2,8 @@ var app = angular.module('appServices', [])
 
 app.factory('Request', ['$http', function($http) {
     return {
-			
+			get : function(){
+                return $http.get('https://hackqc.herokuapp.com/api/JSON');
+            }
         }
 }]);
