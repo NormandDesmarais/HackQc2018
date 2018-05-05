765ba2c39133c13d1ea0e636173a00c4e6e33bba
CP Lepage <lepage.charlesphilippe@gmail.com>
2018-05-05 12:05:34 -0400

    icon UI

--- Frontend/WebApp/app/js/angular/services.js
+++ Frontend/WebApp/app/js/angular/services.js
@@ -2,8 +2,9 @@ var app = angular.module('appServices', [])
 
 app.factory('Request', ['$http', function($http) {
     return {
-			get : function(){
-                return $http.get('https://hackqc.herokuapp.com/api/JSON');
+			get : function(type){
+                if(type == "Inondations")
+                    return $http.get('https://hackqc.herokuapp.com/api/JSON');
             }
         }
 }]);
