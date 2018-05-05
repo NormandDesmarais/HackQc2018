2d689c39cf72fe4a34c9d978b60902eb9d4b6408
Olivier L.-Applin <Applin@iMac-de-Olivier.local>
2018-05-05 00:54:16 -0400

    Server connection with parameters

--- Android/simpleTestApp/app/src/main/java/com/example/payne/simpletestapp/Manager.java
+++ Android/simpleTestApp/app/src/main/java/com/example/payne/simpletestapp/Manager.java
@@ -2,24 +2,24 @@ package com.example.payne.simpletestapp;
 
 import android.util.Log;
 
+import org.json.JSONObject;
+
 public class Manager {
 
     public static final String SERVER_ADDR = "10.240.201.81";
     public static final int PORT = 8080;
-    public ServerConnection server;
-    public static final String test = "https://hackqc.herokuapp.com/api/testPoint";
+    public ServerConnection mainServer;
+    public static final String testURL = "https://hackqc.herokuapp.com/api/testPoint";
     public MainActivity mainActivity;
 
-    public Manager(MainActivity act){
+    public Manager(MainActivity act) throws Exception {
 
         // test server setup
-        ServerConnection mainServer = new ServerConnection(Manager.SERVER_ADDR, Manager.PORT);
+        mainServer = new ServerConnection(Manager.SERVER_ADDR, Manager.PORT);
+        ServerConnection testServer = new ServerConnection(testURL);
 
-        try {
-            String request = server.ping();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        // test
+        JSONObject JSONtest = JSONWrapper.createJSON(testServer.getRequest());
 
     }
 
--- Android/simpleTestApp/app/src/main/java/com/example/payne/simpletestapp/ServerConnection.java
+++ Android/simpleTestApp/app/src/main/java/com/example/payne/simpletestapp/ServerConnection.java
@@ -1,30 +1,26 @@
 package com.example.payne.simpletestapp;
 
-import android.net.Uri;
-import android.util.Log;
-
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 
 public class ServerConnection {
 
-    public String test = "https://hackqc.herokuapp.com/api/testPoint";
-    public String serverAdress;
+    public String serverAddress;
     public int port;
-    public String serverUri;
 
     public ServerConnection(String addr, int port){
-        this.serverAdress = addr;
+        this.serverAddress = addr;
         this.port = port;
-        serverUri = "http://" + Manager.SERVER_ADDR + ":" + Manager.PORT + "/greeting?" ;
+
+    }
+
+    public ServerConnection(String addr){
+
+        this.port = 80;
+        this.serverAddress = addr;
 
     }
 
@@ -35,24 +31,113 @@ public class ServerConnection {
      */
     public String ping() throws Exception {
 
-        String uri = serverUri;
+        String uri = this.serverAddress;
 
         // TODO : add parameter to get request
         uri = uri + "name=toto";
 
-        return this.getRequest(Manager.test);
+        return this.getRequest(this.serverAddress);
 
     }
 
-    public String getRequest(String url) throws Exception {
+    /**
+     *
+     * Une requête pour mettre èa jour l'affichage des zones dangereuse
+     *
+     * @return
+     * @throws Exception
+     */
+    public String request() throws Exception {
+
+        String uri = this.serverAddress;
+
+        // TODO : add parameter to get request
+        uri = uri + "name=toto";
+
+        return this.getRequest(uri);
+
+    }
+
+    /**
+     *
+     * Une requête pour mettre èa jour l'affichage des zones dangereuse
+     *
+     * @return
+     * @throws Exception
+     */
+    public String request(String param) throws Exception {
+
+        String uri = this.serverAddress;
+
+        // TODO : add parameter to get request
+        uri = uri + param;
+
+        return this.getRequest(uri);
+
+    }
+
+
+    /**
+     * Une requpête GET générale
+     *
+     * @return  String  le résultat de la reuête
+     * @throws Exception
+     */
+    public String getRequest() throws Exception {
 
         final Response result = new Response();
 
         Thread connection = new Thread(new Runnable() {
+
             @Override
             public void run() {
+
                 try{
-                    URL obj = new URL(test);
+                    URL obj = new URL(serverAddress);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                    // optional default is GET
+                    con.setRequestMethod("GET");
+                    int responseCode = con.getResponseCode();
+                    // Log.w("response code : ", responseCode + "");
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+
+                    in.close();
+
+                    result.response = response.toString();
+
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        connection.start();
+        connection.join();
+
+
+        return result.response;
+
+    }
+
+    public String getRequest(final String param) throws Exception {
+
+        final Response result = new Response();
+
+        Thread connection = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                try{
+                    URL obj = new URL(serverAddress + param);
                     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
                     // optional default is GET
